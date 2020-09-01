bc7a2bb27a152d5a5305c1ac2382cb5474dfe448
Tahmid Sharif <tahmidsharif@ids.company>
2020-09-01 15:38:20 +0600

    add random song picker

--- api/artist/controllers/artist.js
+++ api/artist/controllers/artist.js
@@ -17,4 +17,31 @@ module.exports = {
     const entity = await strapi.services.artist.findOne({ referral: token	});
     return sanitizeEntity(entity, { model: strapi.models.artist });
   },
+
+  async confirm(ctx) {
+    try {
+      const { token } = ctx.params;
+
+      console.log(token);
+
+        await strapi.query("artist").update(
+          { confirmationToken: token },
+          {
+            confirm: true,
+          }
+        );
+
+        const artist = await strapi.query("artist").findOne({ confirmationToken: token });
+
+       // console.log(artist);
+
+        if(!artist.confirm) throw Error('Not confirmed');
+
+      const entity = artist;
+      return sanitizeEntity(entity, { model: strapi.models.artist });
+    } catch (error) {
+      console.log(error);
+      return {message: "error"}
+    }
+  },
 };
--- api/song/controllers/song.js
+++ api/song/controllers/song.js
@@ -16,14 +16,27 @@ module.exports = {
         .query("artist")
         .findOne({ artist: data.artist_name });
 
+
+
       if (!artist) {
+        const token = crypto.randomBytes(64).toString('hex');
+
         let artistData = {
           artist: data.artist_name,
           referral:
             data.artist_name.slice(0, 3) +
             crypto.randomBytes(6).toString("hex"),
           referred: 0,
+          confirmationToken: token
         };
+
+        await strapi.plugins['email'].services.email.send({
+          to: data.contact_email,
+          from: 'hi@asinglesongreview.com',
+          replyTo: 'hi@asinglesongreview.com',
+          subject: 'Confirm you email',
+          text: 'Please confirm your email using this link ' + process.env.FRONTEND_URL + '/submit-song/confirmation?token=' + token,
+        });
 
         const artistDoc = await strapi.services.artist.create(artistData);
         data.artistId = artistDoc;
@@ -31,18 +44,72 @@ module.exports = {
         data.artistId = artist;
       }
 
-      await strapi.plugins['email'].services.email.send({
-        to: data.contact_email,
-        from: 'hi@asinglesongreview.com',
-        replyTo: 'annesophiepic@strapi.io',
-        subject: 'Use strapi email provider successfully',
-        text: 'Hello ' + data.artist_name,
-      });
-
       entity = await strapi.services.song.create(data, { files });
     } else {
       entity = await strapi.services.song.create(ctx.request.body);
     }
     return sanitizeEntity(entity, { model: strapi.models.song });
   },
+
+  async random() {
+    const artists = await strapi.query('artist').find();
+    const songs = await strapi.query('song').find()
+
+     // console.log(artists);
+
+    const getRandomInt = (max) => {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+
+
+    let song = {};
+
+    const rand = getRandomInt(100);
+
+    async function getRandomsong(arr) {
+      const randSong = getRandomInt(arr.length-1);
+
+      //if(songs[randSong].picked) getRandomsong(arr);
+
+
+      song = songs[randSong];
+
+      // await strapi.query("song").update(
+      //   { id: song.id },
+      //   {
+      //     picked: true,
+      //   }
+      // );
+
+    }
+
+    if(rand >= 85) {
+      getRandomsong(songs);
+    }else {
+      let maxReferred = 0;
+
+      artists.forEach(artist => {
+        maxReferred  += artist.referred;
+      });
+
+      const rand = getRandomInt(100);
+
+      let counted = 0;
+
+      artists.forEach((artist, idx) => {
+        const perc = (artist.referred/maxReferred) * 100;
+
+        if(rand >= counted && rand <= counted + perc) {
+          getRandomsong(artist.songs);
+        }
+
+        counted += perc;
+      })
+    }
+
+
+
+    return sanitizeEntity(song || getRandomsong(songs), { model: strapi.models.song });
+  }
+
 };
