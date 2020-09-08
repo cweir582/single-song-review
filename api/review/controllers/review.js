fb49d8513b0d19597d870b404738cea1f4574598
Tahmid Sharif <tahmidsharif@ids.company>
2020-09-08 15:52:14 +0600

    add azure blob

--- api/review/controllers/review.js
+++ api/review/controllers/review.js
@@ -1,8 +1,41 @@
 'use strict';
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entity, review;
+
+
+    if (ctx.query._q) {
+      //entities = await strapi.services.restaurant.search(ctx.query);
+    } else {
+      entity = await strapi.services.review.find({ sent: true, category: 'songreview' });
+      console.log(entity);
+
+      review = entity.map(item => {
+        const { introduction, like, dontLike, opinion, forfansof, shouldtheylistenmore, id } = item;
+        const { streaming_link, artist_name, song_name, press_photo } = item.song;
+        return {
+          id,
+          introduction,
+          like,
+          dontLike,
+          opinion,
+          forfansof,
+          shouldtheylistenmore,
+          streaming_link,
+          artist_name,
+          song_name,
+          press_photo
+        }
+      })
+    }
+
+    return review;
+  }
+};
--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,24 @@
 module.exports = ({ env }) => ({
-  // ...
+
   email: {
     provider: 'sendgrid',
     providerOptions: {
       apiKey: env('SENDGRID_API_KEY'),
     },
     settings: {
-      defaultFrom: 'juliasedefdjian@strapi.io',
-      defaultReplyTo: 'juliasedefdjian@strapi.io',
+      defaultFrom: 'hi@asinglesongreview.com',
+      defaultReplyTo: 'hi@asinglesongreview.com',
     },
   },
-  // ...
+  upload: {
+    provider: 'azure-storage',
+    providerOptions: {
+      account: env('STORAGE_ACCOUNT'),
+      accountKey: env('STORAGE_ACCOUNT_KEY'),
+      // serviceBaseURL: env('STORAGE_URL'),
+      containerName: env('STORAGE_CONTAINER_NAME'),
+      defaultPath: 'assets',
+      maxConcurrent: 10
+    }
+  }
 });
