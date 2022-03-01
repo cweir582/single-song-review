4bcd6cb45027d7a633b7652b07a2a1d0ac2a8359
UnityTheMaker <unity@themaker.cyou>
2022-03-01 03:31:32 +0300

    add monthly vinyl draw

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST'),
-        srv: env.bool('DATABASE_SRV', true),
+        host: env('DATABASE_HOST', '172.17.0.1'),
+        srv: env.bool('DATABASE_SRV'),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        database: env('DATABASE_NAME', 'single-song'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', 'example'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
